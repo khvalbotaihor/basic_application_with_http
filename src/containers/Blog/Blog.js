5062a805f2766fbb36bcc2ceed53e3713b87f79d
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-10-28 15:12:35 +0200

    - added switch tag

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -5,7 +5,7 @@ import FullPost from './FullPost/FullPost';
 import NewPost from './NewPost/NewPost';
 import './Blog.css';
 import Posts from "./Posts/Posts";
-import {Route, NavLink} from 'react-router-dom'
+import {Route, NavLink, Switch} from 'react-router-dom'
 
 class Blog extends Component {
     render() {
@@ -32,12 +32,15 @@ class Blog extends Component {
                     </nav>
                 </header>
                 <Route path="/" exact component={Posts}/>
-                <Route path="/new-post" component={NewPost}/>
-                <Route path="/posts/:id" exact component={FullPost}/>
+                <Switch>
+                    <Route path="/new-post" component={NewPost}/>
+                    <Route path="/:id" exact component={FullPost}/>
+                </Switch>
+
 
 
                 {/*                <section>
-                    <FullPost id={this.state.selectedPostId}/>
+                    <FullPost id={this.state.selectkedPostId}/>
                 </section>
 */}
 
