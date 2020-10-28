323b07a331047348052bd276172f2b875964673d
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-10-28 16:07:20 +0200

    - add additional auth handling

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -38,7 +38,12 @@ class Blog extends Component {
                 <Switch>
                     {this.state.auth? <Route path="/new-post" component={NewPost}/> : null}
                     <Route path="/posts" component={Posts}/>
+
+                    <Route render={()=><h1>Not found</h1>}/>
+
+{/*
                     <Redirect from="/" to="/posts" />
+*/}
 {/*
                     <Route path="/" component={Posts}/>
 */}
