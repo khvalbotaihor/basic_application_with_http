e63cd3d1c81743f5b91afd5ec572b602c3660751
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-10-28 04:12:57 +0200

    - change return http request data using map function

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -15,7 +15,15 @@ class Blog extends Component {
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(response =>{
-                this.setState({posts: response.data});
+
+                const posts = response.data.slice(0,4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Max'
+                    }
+                })
+                this.setState({posts: updatedPosts});
                 //console.log(response)
             });
     }
