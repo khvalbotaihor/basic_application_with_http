9ed6599988bb8bfef00ba1da046c7dea38ee3f33
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-10-28 06:02:31 +0200

    - return default axios instance

--- src/axios.js
+++ src/axios.js
@@ -3,3 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
     baseURL: 'https://jsonplaceholder.typicode.com'
 })
+
+instance.defaults.headers.common['Authorisation'] = 'AUTH TOCKEN FROM INSTANCE';
+
+export default instance;
