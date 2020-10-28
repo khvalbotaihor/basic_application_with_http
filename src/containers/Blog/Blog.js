68492dd808129d189ea5d9498c7f360db97ad36a
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-10-28 13:14:44 +0200

    - added route to show newpost component

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -19,21 +19,14 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                {/*
-                <Posts/>
-*/}
-                {/*                <Route path="/" exact render={()=> <h1>Home</h1>}/>
-                <Route path="/"  render={()=> <h1>Home 2</h1>}/>*/}
-
                 <Route path="/" exact component={Posts}/>
+                <Route path="/new-post" component={NewPost}/>
 
 
                 {/*                <section>
                     <FullPost id={this.state.selectedPostId}/>
                 </section>
-                <section>
-                    <NewPost />
-                </section>*/}
+*/}
 
 
             </div>
