9e4be95df9ebc113d9d7228df6449053f90f67c6
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-10-28 11:50:38 +0200

    - added browserrouter to our app - added browserrouter to index.js

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -65,12 +65,11 @@ class Blog extends Component {
                 <section className="Posts">
                     {posts}
                 </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId}/>
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+
+
+
+
+
             </div>
         );
     }
