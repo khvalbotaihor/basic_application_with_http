d4c9597d98c37f6cfeff7032b3e4157f612703ed
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-10-28 05:56:11 +0200

    - created axios intance file

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com'
 axios.defaults.headers.common['Authorisation'] = 'AUTH TOCKEN';
+axios.defaults.headers.post['Contet-Type'] = 'application/json';
 
 axios.interceptors.request.use(request => {
     console.log(request);
